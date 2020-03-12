ccd51e155848dc0d75039589875ec2288abb4d08
Nathanael Mario Duarte Delfino <nathanael.delfino@live.com>
2020-03-12 09:54:15 -0300

    :up:  Aula 7

---
+++ src/app.js
@@ -0,0 +1,24 @@
+'user strict'
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+const router = express.Router();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+mongoose.connect('mongodb+srv://balta:balta@ndstr-8vwdy.gcp.mongodb.net/test?retryWrites=true&w=majority');
+
+//Carrega as Rotas
+const index = require('./routes/index-route');
+const products = require('./routes/product-route');
+
+app.use('/', index);
+app.use('/products', products);
+
+
+module.exports = app;
