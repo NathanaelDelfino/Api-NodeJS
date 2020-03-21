f00c668f7469b17d9fc2d5e6a6ff922c9cf4ad0c
Nathanael Mario Duarte Delfino <nathanael.delfino@live.com>
2020-03-21 16:51:03 -0300

    :up:  Balta.io - 1972 - Concluído

--- src/app.js
+++ src/app.js
@@ -6,10 +6,21 @@ const config = require('./config')
 const app = express();
 //const router = express.Router();
 
-app.use(bodyParser.json());
+app.use(bodyParser.json({
+  limit:'5mb'
+}));
 app.use(bodyParser.urlencoded({
   extended: false
 }));
+
+
+// Habilita o CORS
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  next();
+});
 
 mongoose.connect(config.connectionString);
 
